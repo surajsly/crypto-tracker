b606e22c7dbba8aa4d13c073f1c14816bffe56f6
Suraj Singh <50519406+surajsly@users.noreply.github.com>
2020-10-01 23:39:44 +0530

    axios added and API request done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
-import React from 'react';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
+import "./App.css";
+
+/*
+https://api.coingecko.com/api/v3/coins/markets?vs_currency=inr&order=market_cap_desc&per_page=10&page=1&sparkline=false
+ */
 function App() {
+  const [Coins, setCoins] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(
+        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=inr&order=market_cap_desc&per_page=10&page=1&sparkline=false"
+      )
+      .then((res) => {
+        setCoins(res.data);
+      })
+      .catch((error) => console.log(error));
+  }, []);
+
   return (
     <div className="App">
       <p>hii</p>
