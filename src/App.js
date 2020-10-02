4932907de32f693bfd02225a92942d649e449e59
Suraj Singh <50519406+surajsly@users.noreply.github.com>
2020-10-02 22:46:02 +0530

    editing title and pushing build

--- src/App.js
+++ src/App.js
@@ -4,16 +4,13 @@ import Coin from "./Coins";
 
 import "./App.css";
 
-/*
-https://api.coingecko.com/api/v3/coins/markets?vs_currency=inr&order=market_cap_desc&per_page=10&page=1&sparkline=false
- */
 function App() {
   const [Coins, setCoins] = useState([]);
   const [search, setSearch] = useState("");
   useEffect(() => {
     axios
       .get(
-        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=inr&order=market_cap_desc&per_page=10&page=1&sparkline=false"
+        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=inr&order=market_cap_desc&per_page=20&page=1&sparkline=false"
       )
       .then((res) => {
         setCoins(res.data);
