ea36fe8f7cdf615d91862ad7799b8e0fbf478bb7
Suraj Singh <50519406+surajsly@users.noreply.github.com>
2020-10-02 19:54:48 +0530

    mapping data from API

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import Coin from "./Coins";
 
 import "./App.css";
 
@@ -8,7 +9,7 @@ https://api.coingecko.com/api/v3/coins/markets?vs_currency=inr&order=market_cap_
  */
 function App() {
   const [Coins, setCoins] = useState([]);
-
+  const [search, setSearch] = useState("");
   useEffect(() => {
     axios
       .get(
@@ -20,9 +21,41 @@ function App() {
       .catch((error) => console.log(error));
   }, []);
 
+  const handleChange = (e) => {
+    setSearch(e.target.value);
+  };
+
+  const filteredCoins = Coins.filter((coin) =>
+    coin.name.toLowerCase().includes(search.toLowerCase())
+  );
+
   return (
-    <div className="App">
-      <p>hii</p>
+    <div className="coin-app">
+      <div className="coin-search">
+        <h1 className="text">Search a currency</h1>
+        <form>
+          <input
+            onChange={handleChange}
+            type="text"
+            className="coin-input"
+            placeholder="Search"
+          />
+        </form>
+      </div>
+      {filteredCoins.map((coin) => {
+        return (
+          <Coin
+            key={coin.id}
+            name={coin.name}
+            symbol={coin.symbol}
+            image={coin.image}
+            marketcap={coin.market_cap}
+            price={coin.current_price}
+            priceChange={coin.price_change_percentage_24h}
+            volume={coin.total_volume}
+          />
+        );
+      })}
     </div>
   );
 }
