b1157439a5aa4a0be86c528fcf3770e975a0a753
Suraj Singh <50519406+surajsly@users.noreply.github.com>
2020-10-02 23:05:15 +0530

    Updates

---
+++ precache-manifest.25a6ec98f849651f739444a58ff704e3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10f7ba1b4d5afb7b6d4581f183721994",
+    "url": "/index.html"
+  },
+  {
+    "revision": "67a04d4e9722449bf6ef",
+    "url": "/static/css/main.5f94eeb8.chunk.css"
+  },
+  {
+    "revision": "e042156ecda685a4d548",
+    "url": "/static/js/2.86bab506.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.86bab506.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67a04d4e9722449bf6ef",
+    "url": "/static/js/main.4a515ae5.chunk.js"
+  },
+  {
+    "revision": "392e57d1358b9e5f592d",
+    "url": "/static/js/runtime-main.69a76cdf.js"
+  }
+]);
